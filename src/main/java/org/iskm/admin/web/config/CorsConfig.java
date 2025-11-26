cbddbe2d6a6d4a020a0d889f7ea525092be2e68b
kumarcharan00 <kumarcharan463@gmail.com>
2025-11-27 01:28:41 +0530

    railyway deployment fix

--- src/main/java/org/iskm/admin/web/config/CorsConfig.java
+++ src/main/java/org/iskm/admin/web/config/CorsConfig.java
@@ -2,6 +2,7 @@ package org.iskm.admin.web.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,8 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    //  @Value("${allowed.endpoints.list}") // optional property, empty by default
-    private String allowedOrigins = "";
+    @Value("${allowed.endpoints.list}") // optional property, empty by default
+    private String allowedOrigins;
 
     @Override
     @SuppressWarnings("null")
