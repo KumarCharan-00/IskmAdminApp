5f657aa15d047e23562a2d18e92439861c6a836d
kumarcharan00 <kumarcharan463@gmail.com>
2025-11-30 11:00:56 +0530

    Moding Request Mapping

--- src/main/java/org/iskm/admin/web/controller/SponsorsController.java
+++ src/main/java/org/iskm/admin/web/controller/SponsorsController.java
@@ -8,31 +8,33 @@ import org.iskm.admin.web.service.SponsorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class SponsorsController {
-	
-	private final SponsorService sponsorService;
+
+    private final SponsorService sponsorService;
 
     public SponsorsController(SponsorService sponsorService) {
         this.sponsorService = sponsorService;
     }
-	
-	@PostMapping("/generate-pay-req")
+
+    @PostMapping("/generate-pay-req")
     public ResponseEntity<RPPaymentRequest> addSponser(@RequestBody OrderRequestDetails req) {
-		log.info("Generating payment request for donor: {}", req);
+        log.info("Generating payment request for donor: {}", req);
         return ResponseEntity.ok(sponsorService.generatePaymentRequest(req));
     }
-	
-	@PostMapping("/payment-callback")
-	public String paymentCallback(@RequestParam Map<String,String> response) {
-		sponsorService.updateStatus(response);
-		return "Success";
-		
-	}
+
+    @PostMapping("/payment-callback")
+    public String paymentCallback(@RequestParam Map<String, String> response) {
+        sponsorService.updateStatus(response);
+        return "Success";
+
+    }
 }
